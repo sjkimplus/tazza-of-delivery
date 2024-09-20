82483ca9daee058b6c1d9b9df5711f4307205a87
Yehwan An <168810508+davidan94@users.noreply.github.com>
2024-09-20 20:55:14 +0900

    test

---
+++ src/main/java/com/sparta/tazzaofdelivery/domain/review/entity/Review.java
@@ -0,0 +1,11 @@
+package com.sparta.tazzaofdelivery.domain.review.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Review {
+}
