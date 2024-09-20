ace1e21af4f15a349b4feef145c4909324da4056
Sungju Kim <sjkim20010909@gmail.com>
2024-09-20 18:41:59 +0900

    [FEAT] user entity + auth annotation + authArgumentResolver 추가

--- src/main/java/com/sparta/tazzaofdelivery/config/webconfig/WebConfig.java
+++ src/main/java/com/sparta/tazzaofdelivery/config/webconfig/WebConfig.java
@@ -1,4 +1,20 @@
 package com.sparta.tazzaofdelivery.config.webconfig;
 
-public class WebConfig {
+import com.sparta.tazzaofdelivery.config.authconfig.AuthArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    // ArgumentResolver 등록
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthArgumentResolver());
+    }
 }
---
+++ src/main/java/com/sparta/tazzaofdelivery/domain/user/UserStatus.java
@@ -0,0 +1,21 @@
+package com.sparta.tazzaofdelivery.domain.user;
+
+public enum UserStatus {
+    ACTIVE(UserStatus.Authority.ACTIVE),
+    DELETED(UserStatus.Authority.DELETED);
+
+    private final String authority;
+
+    UserStatus(String authority) {
+        this.authority = authority;
+    }
+
+    public String getUserStatus() {
+        return this.authority;
+    }
+
+    public static class Authority {
+        public static final String ACTIVE = "USER_ACTIVE";
+        public static final String DELETED = "USER_DELETED";
+    }
+}
