8e6f1e98e1b3ce1900b587221ea70bc524315b92
kang-sumin <min001117@gmail.com>
2024-09-25 10:06:10 +0900

    [FIX] 활성화된 cart 여부 확인 로직 추가

--- src/main/java/com/sparta/tazzaofdelivery/domain/cart/repository/CartRepository.java
+++ src/main/java/com/sparta/tazzaofdelivery/domain/cart/repository/CartRepository.java
@@ -1,13 +1,20 @@
 package com.sparta.tazzaofdelivery.domain.cart.repository;
 
 import com.sparta.tazzaofdelivery.domain.cart.entity.Cart;
+import com.sparta.tazzaofdelivery.domain.cart.entity.CartStatus;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
 
-
+    @Query("SELECT c FROM Cart c WHERE c.userId = :userId AND c.cartStatus = :cartStatus")
+    Optional<List<Cart>> findValidCartsByUserId(@Param("userId")Long userId, @Param("cartStatus")CartStatus cartStatus);
 }
